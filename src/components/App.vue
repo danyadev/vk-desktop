d4419b8d4205123cffe0db14c3f28d557465d703
danyadev <danyadev@mail.ru>
2019-03-11 14:17:14 +0300

    font

--- src/components/App.vue
+++ src/components/App.vue
@@ -18,8 +18,24 @@
 
   *, *::before, *::after { box-sizing: border-box }
 
+  @font-face {
+  	font-family: 'Segoe UI';
+  	font-weight: 400;
+  	font-display: block;
+  	src: local('Segoe UI'),
+  			 url('/dist/assets/SegoeUI.ttf');
+  }
+
+  @font-face {
+  	font-family: 'Segoe UI';
+  	font-weight: 500;
+  	font-display: block;
+  	src: local('Segoe UI Semibold'),
+  			 url('/dist/assets/SegoeUISemiBold.ttf');
+  }
+
   body {
-    font-family: BlinkMacSystemFont, sans-serif;
+    font-family: BlinkMacSystemFont, 'Segoe UI';
     font-size: 15px;
     margin: 0;
     overflow: hidden;
