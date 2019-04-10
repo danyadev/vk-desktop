5d42cef8f56d4225903046ea82442894a9aa0d8d
danyadev <danyadev@mail.ru>
2019-04-10 16:38:28 +0300

    Удалил папку со старым кодом проекта

--- renderer/components/messages/ChatEmpty.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="dialog_container">
-    <div class="header"></div>
-    <div class="dialog_choice_chat">
-      <img src="images/im_choice_chat.png">
-      {{ l('im_choose_chat') }}
-    </div>
-  </div>
-</template>
