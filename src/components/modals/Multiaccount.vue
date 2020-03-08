9bd6dda06a7597de14104879b6c3e0bc55a4e3da
danyadev <danyadev@mail.ru>
2020-03-08 22:42:49 +0300

    Теперь при смене аккаунта перед перезагрузкой ничего не меняется

--- src/components/modals/Multiaccount.vue
+++ src/components/modals/Multiaccount.vue
@@ -25,6 +25,7 @@
 
 <script>
   import { mapState } from 'vuex';
+  import { usersStorage } from 'js/store/Storage';
 
   import ModalHeader from './ModalHeader.vue';
   import Button from '../UI/Button.vue';
@@ -49,9 +50,16 @@
         const prevUser = this.activeUser;
         if(prevUser == id) return;
 
-        this.$store.commit('users/setActiveUser', id);
+        if(prevUser) {
+          const usersData = Object.assign({}, usersStorage.data);
 
-        if(prevUser) location.reload();
+          usersData.activeUser = id;
+          usersStorage.update(usersData);
+
+          location.reload();
+        } else {
+          this.$store.commit('users/setActiveUser', id);
+        }
       },
 
       removeAccount(id) {
