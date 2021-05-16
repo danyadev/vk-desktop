8df787bbca58ce52793f15676ef02be43ad4e924
danyadev <danyadev@mail.ru>
2021-05-16 08:35:31 +0300

    chore: lint

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
+import os from 'os';
+import path from 'path';
 import { EventEmitter } from 'events';
 import { reactive } from 'vue';
 import electron from 'electron';
-import os from 'os';
-import path from 'path';
 import pkg from '../../package.json';
 import { usersStorage } from './store/Storage';
 import request from './request';
