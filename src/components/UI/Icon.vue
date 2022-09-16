27c07d43f5da5c721770344336bb797325801d40
danyadev <danyadev@mail.ru>
2022-09-16 20:52:12 +0300

    refactor: delete all code

--- src/components/UI/Icon.vue
+++
@@ -1,6 +0,0 @@
-<script>
-export default function Icon({ name, ...iconProps }) {
-  const Icon = require(`!vue-svg-loader!assets/${name}.svg`).default;
-  return <Icon {...iconProps} />;
-}
-</script>
