c0fbe43fef7b314071dd127364c541c2d99b9711
danyadev <danyadev@mail.ru>
2023-04-09 15:45:25 +0300

    feat: скелет мессенджера

--- src/ui/messenger/Messenger/Messenger.tsx
+++ src/ui/messenger/Messenger/Messenger.tsx
@@ -4,7 +4,17 @@ import { defineComponent } from 'vue'
 export const Messenger = defineComponent(() => {
   return () => (
     <div class="Messenger">
-      масажор 3000
+      <div class="Messenger__leftPanel">
+        <div class="Messenger__leftPanelHeader">
+          шапка
+        </div>
+        <div class="Messenger__leftPanelContent">
+          список диалогов
+        </div>
+      </div>
+      <div class="Messenger__content">
+        контент
+      </div>
     </div>
   )
 })
