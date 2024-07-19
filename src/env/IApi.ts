e49c223f53da059a6124e5e9ce4783c62ce327fb
Danil Nemov <d.nemov@vk.team>
2024-07-19 09:09:31 +0300

    refactor: use classes for api errors

--- src/env/IApi.ts
+++ src/env/IApi.ts
@@ -12,39 +12,15 @@ type ResultError = {
   captcha_img?: string
   request_params: RequestParams
 }
-type ResultExecuteErrors = Array<{
+type ResultExecuteErrors = ResultExecuteError[]
+type ResultExecuteError = {
   method: string
   error_code: number
   error_msg: string
   captcha_sid?: string
   captcha_img?: string
-}>
+}
 type RequestParams = Array<{ key: string, value: string }>
-
-export type Error = FetchError | MethodError | ExecuteError
-export type FetchError = {
-  type: 'FetchError'
-  kind: 'ServerError' | 'NetworkError'
-  payload: unknown
-}
-export type MethodError = {
-  type: 'MethodError'
-  code: number
-  message: string
-  captchaSid?: string
-  captchaImg?: string
-  requestParams: RequestParams
-}
-export type ExecuteError = {
-  type: 'ExecuteError'
-  errors: NonEmptyArray<{
-    method: string
-    code: number
-    message: string
-    captchaSid?: string
-    captchaImg?: string
-  }>
-}
 
 export type MethodParams<Method extends keyof Methods> = Methods[Method]['params'] & CommonParams
 
@@ -62,6 +38,28 @@ export type FetchOptions = {
   retries?: number
   timeout?: number
   headers?: Record<string, string>
+}
+
+export class FetchError extends Error {
+  constructor(public kind: 'ServerError' | 'NetworkError', public reason: unknown) {
+    super(`[API] Fetch error: ${kind}`, { cause: reason })
+  }
+}
+
+export class MethodError extends Error {
+  public code: number
+
+  constructor(public method: string, public apiError: ResultError) {
+    super(`[API] ${method} error: ${apiError.error_code} ${apiError.error_msg}`)
+    this.code = apiError.error_code
+  }
+}
+
+export class ExecuteError extends Error {
+  constructor(public errors: NonEmptyArray<ResultExecuteError>) {
+    const methodsList = errors.map(({ method }) => method).join(', ')
+    super(`[API] Execute error with methods: ${methodsList}`)
+  }
 }
 
 export interface Api {
@@ -89,6 +87,4 @@ export interface Api {
     method: Method,
     params?: MethodParams<Method>
   ): [Method, MethodParams<Method>]
-
-  isApiError(error: unknown): error is Error
 }
