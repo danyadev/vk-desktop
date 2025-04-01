1f6d9dceeac841bdb5b14429ae6ddf265b1fc5c1
Binatik <sadxas999@mail.ru>
2025-04-02 02:50:59 +0300

    feat: open storybook in electron

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {
+      docgen: 'vue-component-meta',
+    },
+  },
+  async viteFinal(config) {
+    // Добавляем поддержку JSX
+    config.plugins = config.plugins || [];
+    config.plugins.push({
+      name: 'enable-jsx',
+      config() {
+        return {
+          esbuild: {
+            jsx: 'automatic',
+          },
+        };
+      },
+    });
+    return config;
+  },
+};
+
+export default config;
---
+++ src/ui/ui/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+// import { Button } from './Button'
+// import './Button.css'
+
+// import type { Meta, StoryObj } from '@storybook/vue3'
+
+// const meta = {
+//   component: Button,
+//   title: 'UI/Button',
+//   tags: ['autodocs']
+
+// } satisfies Meta<typeof Button>
+
+// export default meta
+// type Story = StoryObj<typeof meta>
+
+// export const Small: Story = {
+//   args: {
+//     size: 'small'
+//   }
+// }
