1234f194f6d2851794cb3ac41f0035535bb059d2
TinySmallM <sadxas999@mail.ru>
2025-04-15 22:37:05 +0300

    feat: rollback

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/vue3-vite',
     options: {
--- src/ui/ui/Button/Button.stories.tsx
+++ src/ui/ui/Button/Button.stories.tsx
@@ -3,18 +3,27 @@
 
 // import type { Meta, StoryObj } from '@storybook/vue3'
 
-// const meta = {
+// export const meta: Meta<typeof Button> = {
 //   component: Button,
-//   title: 'UI/Button',
-//   tags: ['autodocs']
+//   title: 'Components/Button'
+// }
 
-// } satisfies Meta<typeof Button>
-
-// export default meta
-// type Story = StoryObj<typeof meta>
+// type Story = StoryObj<typeof Button>
 
 // export const Small: Story = {
 //   args: {
 //     size: 'small'
+//   }
+// }
+
+// export const Medium: Story = {
+//   args: {
+//     size: 'medium'
+//   }
+// }
+
+// export const Large: Story = {
+//   args: {
+//     size: 'large'
 //   }
 // }
