e1ef3c76f9fdcb50ba85e97e5ab88c73c6525273
danyadev <danyadev@mail.ru>
2025-08-10 12:59:46 +0300

    refactor: simplified photos layout

--- src/misc/providers.ts
+++ src/misc/providers.ts
@@ -1,16 +1,16 @@
-import { inject, InjectionKey, Ref } from 'vue'
-
-type ConvoHistoryContext = {
-  historyViewportWidth: Ref<number>
-  historyViewportHeight: Ref<number>
-}
-
-export const convoHistoryContextInjectKey = Symbol() as InjectionKey<ConvoHistoryContext>
-
-export const useConvoHistoryContext = () => {
-  const context = inject(convoHistoryContextInjectKey)
-  if (!context) {
-    throw new Error('No convoHistoryContext provided')
-  }
-  return context
-}
+// import { inject, InjectionKey, Ref } from 'vue'
+//
+// type ConvoHistoryContext = {
+//   historyViewportWidth: Ref<number>
+//   historyViewportHeight: Ref<number>
+// }
+//
+// export const convoHistoryContextInjectKey = Symbol() as InjectionKey<ConvoHistoryContext>
+//
+// export const useConvoHistoryContext = () => {
+//   const context = inject(convoHistoryContextInjectKey)
+//   if (!context) {
+//     throw new Error('No convoHistoryContext provided')
+//   }
+//   return context
+// }
